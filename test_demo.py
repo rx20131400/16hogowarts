a5f2ca2815f8384bf56b541f7d4009bf9ceaa520
rx20131400 <75324133@qq.com>
2020-12-16 22:36:48 +0800

    第二次提交

--- test_demo.py
+++ test_demo.py
@@ -2,23 +2,15 @@
 # -*- coding: utf-8 -*-
 import pytest
 class Test_demo():
-
-    def test_one(self):
-        a = 5
-        b = 1
-
-        assert a != b
-
-        print("这是我第一个测试用例")
+    @pytest.mark.demo
+    def test_demo(self):
+        print("我的第一个用例")
 
 
-    def add_function(a,b):
-        return a + b
+    @pytest.mark.demo
+    @pytest.mark.smoke
+    def test_two(self):
+        print("我的第二个用例")
 
-@pytest.mark.parametrize("a",[0,1])
-@pytest.mark.parametrize("b",[2,4])
-
-def test_add(a,b):
-        print("测试数据组合： a > %s, b > %s"%(a,b))
 
 
